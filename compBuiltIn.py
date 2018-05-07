32c02b391c7d47dfd3b42c153028faa8e23bc40f
rcpsilva <rcpsilva@gmail.com>
2018-05-07 14:07:20 -0300

    Affinity Propagation 2 with loops
    Affinity Propagation 2 with loops for readability

--- compBuiltIn.py
+++ compBuiltIn.py
@@ -4,12 +4,11 @@ from sklearn.datasets.samples_generator import make_blobs
 import create_database as cd
 import affinityPropagationClustering as myaff
 
-
 centers = [[1, 1], [-1, -1], [1, -1]]
-data, labels_true = make_blobs(n_samples=300, centers=centers, cluster_std=0.5,
-                            random_state=0)
-
-#data = cd.generateTData(5)
+data, labels_true = make_blobs(n_samples=300, centers=centers, cluster_std=0.5, random_state=0)
+#print(data)
+#data = cd.generateTData(100)
+#data = data[:,[0,2]]
 
 # Compute Affinity Propagation
 af = AffinityPropagation(preference=-50).fit(data)
@@ -20,9 +19,12 @@ n_clusters_ = len(cluster_centers_indices)
 
 print('Scikit Results')
 print(labels)
+print(cluster_centers_indices)
 
-(c,exemplars) = myaff.affinityPropagationR(data,myaff.inverseEuclid)
 
+(c,exemplars) = myaff.affinityPropagationR2(data,myaff.inverseEuclid,lam=0.5,maxiter=100)
+
+#print(data)
 print('My Results')
 print(c)
 print(exemplars)
@@ -31,3 +33,4 @@ print(exemplars)
 #    plt.plot(data[c==i,1],data[c==i,2],'ro')
 
 #plt.show()  
+
